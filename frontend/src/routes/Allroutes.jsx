bf58074de196533b33f8c17eb955d4316d2fda96
Ansh Girdhar <anshgirdhar20@gmail.com>
2022-09-28 23:07:18 +0530

    Update Allroutes.jsx

--- frontend/src/routes/Allroutes.jsx
+++ frontend/src/routes/Allroutes.jsx
@@ -4,8 +4,10 @@ import Login from '../pages/Login'
 import Error from '../pages/Error'
 
 export const Allroutes = () => {
-  return  <Routes>
-  <Route path="/login" element={<Login />} />
-  <Route path="*" element={<Error />} />
-</Routes>
+  return (
+    <Routes>
+    <Route path="/login" element={<Login />} />
+    <Route path="*" element={<Error />} />
+    </Routes>
+)
 };
