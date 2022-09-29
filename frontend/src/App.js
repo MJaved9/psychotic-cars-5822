c14768cbde51a2751b4ab1f57d99bc8a562f2731
haresh2349 <hareshcsolanki07@gmail.com>
2022-09-30 00:03:23 +0530

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,15 @@
 import { Box } from "@chakra-ui/react";
-import { Pricing } from "./pages/Pricing";
 
 function App() {
   return (
-    //   <Box
-    //   className="App"
-    //   display="flex"
-    //   style={{
-    //     background:
-    //       "url('https://b24-1r41i2.bitrix24.com/bitrix/templates/bitrix24/themes/light/atmosphere/atmosphere2.jpg')",
-    //   }}
-    // ></Box>
-    <Pricing />
+    <Box
+      className="App"
+      display="flex"
+      style={{
+        background:
+          "url('https://b24-1r41i2.bitrix24.com/bitrix/templates/bitrix24/themes/light/atmosphere/atmosphere2.jpg')",
+      }}
+    ></Box>
   );
 }
 
