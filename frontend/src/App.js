bda4da8b87c457f491a2e47711d5e2e65550fe85
AnshGirdhar1 <anshgirdhar20@gmail.com>
2022-09-29 23:33:48 +0530

    Created Redux Store for Team Members Page and Drawer for Adding Team Members

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,15 @@
-
-import {Box} from '@chakra-ui/react'
+import { Box } from "@chakra-ui/react";
 
 function App() {
   return (
     <Box
-    className="App"
-    display="flex"
-    style={{
-      background:
-        "url('https://b24-1r41i2.bitrix24.com/bitrix/templates/bitrix24/themes/light/atmosphere/atmosphere2.jpg')",
-    }}
-  ></Box>
-
+      className="App"
+      display="flex"
+      style={{
+        background:
+          "url('https://b24-1r41i2.bitrix24.com/bitrix/templates/bitrix24/themes/light/atmosphere/atmosphere2.jpg')",
+      }}
+    ></Box>
   );
 }
 
