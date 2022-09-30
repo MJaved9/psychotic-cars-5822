5a287822abb53791223ac0fc2c61f39a0d69db32
AnshGirdhar1 <anshgirdhar20@gmail.com>
2022-09-30 17:28:20 +0530

    Updated Task Projects and Teams Route

--- backend/routes/projects.route.js
+++ backend/routes/projects.route.js
@@ -1,55 +1,60 @@
 const { Router } = require("express");
+const { authentication } = require("../middlewares/authentication");
 const { ProjectModel } = require("../models/projects.model");
 
 const ProjectRouter = Router();
 
-ProjectRouter.get("/getProjects", async (req, res) => {
+ProjectRouter.get("/getProjects", authentication, async (req, res) => {
   try {
-    const userId = req.user_data._id;
+    const { userId } = req.body;
     const project = await ProjectModel.find({ userId });
-    tasks.save();
-    return res.status(201).send({ type: "success", project: project });
+    res.status(201).send({ type: "success", project: project });
   } catch (error) {
-    return res.status(500).send({ type: "error", message: "An error occured" });
+    res.status(500).send({ type: "error", message: "An error occured" });
   }
 });
 
-ProjectRouter.post("/create", async (req, res) => {
+ProjectRouter.post("/create", authentication, async (req, res) => {
+  const { userId } = req.body;
   try {
-    const userId = req.user_data._id;
-    const new_project = await new ProjectModel(...req.body, userId);
-    new_project.save();
-    return res
-      .status(201)
-      .send({ type: "success", message: "Project is created" });
+    const new_project = new ProjectModel({ ...req.body, userId });
+    await new_project.save();
+    res.status(201).send({ type: "success", message: "Project is created" });
   } catch (error) {
-    return res.status(500).send({ type: "error", message: "An error occured" });
+    res.status(500).send({ type: "error", message: "An error occured" });
   }
 });
 
-ProjectRouter.patch("/edit/:id", async (req, res) => {
+ProjectRouter.patch("/edit/:id", authentication, async (req, res) => {
   const { id } = req.params;
+  const { userId } = req.body;
   try {
-    const updatedProject = ProjectModel.finfByIdAndUpdate(id, req.body);
-    updatedProject.save();
-    return res
-      .status(201)
-      .send({ type: "success", message: "Project is updated" });
+    const updatedProject = await ProjectModel.findOneAndUpdate(
+      { _id: id, userId },
+      req.body
+    );
+    if (updatedProject) {
+      res.status(201).send({ type: "success", message: "Project is updated" });
+    } else {
+      res.status(500).send({ type: "error", message: "An error occured" });
+    }
   } catch (error) {
-    return res.status(500).send({ type: "error", message: "An error occured" });
+    res.status(500).send({ type: "error", message: "An error occured" });
   }
 });
 
-ProjectRouter.delete("/delete/:id", async (req, res) => {
+ProjectRouter.delete("/delete/:id", authentication, async (req, res) => {
   const { id } = req.params;
+  const { userId } = req.body;
   try {
-    const remaiProjects = ProjectModel.finfByIdAndUpdate(id, { delete: true });
-    remaiProjects.save();
-    return res
-      .status(201)
-      .send({ type: "success", message: "Project is deleted" });
+    const deleted = await ProjectModel.findOneAndDelete({ _id: id, userId });
+    if (deleted) {
+      res.status(201).send({ type: "success", message: "Project is deleted" });
+    } else {
+      res.status(500).send({ type: "error", message: "An error occured" });
+    }
   } catch (error) {
-    return res.status(500).send({ type: "error", message: "An error occured" });
+    res.status(500).send({ type: "error", message: "An error occured" });
   }
 });
 
