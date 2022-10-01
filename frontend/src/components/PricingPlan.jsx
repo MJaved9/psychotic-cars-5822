a512d41ac1b7eb2249d1d9ff7f5de3b23b7392f7
haresh2349 <hareshcsolanki07@gmail.com>
2022-10-01 21:31:28 +0530

    pricing page updated

--- frontend/src/components/PricingPlan.jsx
+++ frontend/src/components/PricingPlan.jsx
@@ -8,46 +8,85 @@ const PricingPlan = ({
   users,
   strikePrice,
   price,
-  color,
   buttonText,
   buttonBg,
   priceColor,
   storage,
+  storageColor,
+  vStackBg,
+  collabSpan,
+  categories,
 }) => {
+  console.log(buttonBg);
   return (
     <div className={styles.planContainer}>
       <div className={`${styles[headBg]} ${styles.planHead}`}>
         <h3>{heading}</h3>
         <p>{headText}</p>
       </div>
-      <div>
+      <div className={`${styles[vStackBg]} ${styles.vStack}`}>
         <div className={`${styles.planUsers}`}>
           <div>
-            <div>{heading && <IoFlameSharp />}</div>
+            <div>
+              {(heading == "Free" || heading == "Professional") && (
+                <IoFlameSharp />
+              )}
+            </div>
             <h3>{users}</h3>
           </div>
           <p>users</p>
         </div>
         <div className={styles.price}>
           {heading == "Free" ? (
-            <h2 className={`${styles[priceColor]}`}>100% free</h2>
+            <h2 className={`${styles[priceColor]} ${styles.onlyHundred}`}>
+              100% free
+            </h2>
           ) : (
-            <div>
-              <s>Rs {strikePrice}/mo</s>
-              <h2 className={`${styles[priceColor]}`}>
-                Rs. {price}/mo <span>-20%</span>
-              </h2>
-            </div>
+            <>
+              <div>
+                <s>Rs {strikePrice}/mo</s>
+                <h2 className={`${styles[priceColor]}`}>
+                  Rs. {price}/mo <span>-20%</span>
+                </h2>
+              </div>
+              <div>
+                <p>for all user</p>
+              </div>
+            </>
           )}
-          <div>
-            <p>for all user</p>
-          </div>
         </div>
-        <div>
+        <div className={`${styles.btnDiv}`}>
           <button className={`${styles[buttonBg]}`}>{buttonText}</button>
         </div>
-        <div className={`${styles[storage]}`}>
+        <div className={`${styles[storageColor]}`}>
           <h3>{storage}</h3>
+        </div>
+        <div className={styles.collobBox}>
+          <div>
+            <h3>Collaboration</h3>
+            {/* <div className={styles.collobSpans}>
+                        for(let i = 0;i < spans;i++){
+                            <span></span>
+                        }
+                </div> */}
+          </div>
+          <div className={styles.collabTags}>
+            <p>Chat</p>
+            <p>HD Videocalls</p>
+            <p>Calendar</p>
+            <p>Company workspace</p>
+            <p>Feed</p>
+            <p>Knowledge base</p>
+          </div>
+        </div>
+        <div className={styles.categories}>
+          {categories.map((el) => {
+            return (
+              <div>
+                <h3>{el}</h3>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
