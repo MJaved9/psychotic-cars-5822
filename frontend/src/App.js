ffb8c405ffaf77650e8b1e2f96efbff253ab9a5d
haresh2349 <hareshcsolanki07@gmail.com>
2022-10-02 16:19:49 +0530

    final commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,8 @@ function App() {
           "url('https://b24-1r41i2.bitrix24.com/bitrix/templates/bitrix24/themes/light/atmosphere/atmosphere2.jpg')",
       }}
     >
-      <Allroutes />
+      {/* <Allroutes /> */}
+      <Pricing />
     </Box>
   );
 }
